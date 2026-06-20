c6d4f2936e98ecf6412760be76a5102bb4e7288c
Cherinho11 <marcellagrau6@gmail.com>
2026-06-20 14:28:10 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+let sensor = prompt("Informe a temperatura do sensor")
+if ( sensor >= 0) {
+    alert("O valorregistraddo é positivo")
+} else if (sensor < 0) {
+    alert(" o valor registrado é negativo")
+} else {
+    alert(" o valor registrado é zero")
+}
